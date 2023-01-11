6eeecaf1b845e65531df8b4aa10e56819dc42f3e
maqiie <99504010+maqiie@users.noreply.github.com>
2023-01-11 08:48:22 +0300

    done

---
+++ src/components/todoItems.js
@@ -0,0 +1,9 @@
+function Todoitems(){
+    return(
+        <div>
+            <h1>todo items</h1>
+        </div>
+    )
+}
+
+export default Todoitems;
---
+++ src/components/todolist.js
@@ -0,0 +1,13 @@
+import Todoitems from "./todoItems";
+
+function Todolist(){
+    return(
+        <div>
+            <p>This is my todolist</p>
+            <Todoitems />
+            
+        </div>
+    )
+}
+
+export default Todolist;
