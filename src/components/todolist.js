989fe65a0fe16bb19375893a1321b9814d1985b0
maqiie <99504010+maqiie@users.noreply.github.com>
2023-01-11 09:31:27 +0300

    exported todoitems

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -3,7 +3,7 @@ import Todoitems from "./todoItems";
 function Todolist(){
     return(
         <div>
-            <p>This is my  updated todolist</p>
+            <p>This Is my  updated todolist</p>
             <Todoitems />
             
         </div>
