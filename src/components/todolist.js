64b150978341ec8ae9a0d9e2eeb4cf5da72f7fac
maqiie <99504010+maqiie@users.noreply.github.com>
2023-01-17 11:58:51 +0300

    working on todo app

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Todoitems from "./todoItems";
 
 function Todolist(){
+
+    const[todos,seTodo] =useState([
+        {
+            "id":1,
+            "title":"learn react",
+            "description":"introductions to react"
+        },
+        {
+            "id":2,
+            "title":"go to the market",
+            "description":"buy some groceries "
+        }
+    ])
     return(
-        <div>
-            <p>This Is my  updated todolist</p>
-            <Todoitems />
-            
-        </div>
-    )
+            <div>
+                {/*loop over todo list and get individual todo*/}
+    {
+        todos.map(item, index) => (
+            <Todoitems key={index} item=(item) />
+    }   
+    ))
+
+</div>
+   
 }
 
 export default Todolist;
--- src/views/home.js
+++ src/views/home.js
@@ -1,14 +1,76 @@
+import { useState } from "react";
 import Todolist from "../components/todolist"
 
 function home(){
+const [todo, seTodo] = useState([
+ {
+        id: 1,
+        "title":
+        "description":"",
+        "status":"",
+},
+{
+    id: 1,
+    "title":
+    "description":"learning react",
+    "status": false
+},
+{
+    id: 1,
+    "title":
+    "description":"",
+    "status":"",
+}
+
+])
+
+// new todo state
+const [newTodo, setNewTodo]=useState("")
+// function add new todo
+const handleSubmit =()=>{}
+e.preventDefault()
+
+const newTask ={
+    id: Math.random() +1,
+    "title":
+    "description":"",
+    "status":"",
+}
+seTodo([...todo,newTask])
+
+
+}
+
+//handle input change
+const handleChange = (e)=>{
+    setNewTodo(e.target.value)
+    console.log(newTodo)
+
+}
+// e allows us to get the content bieng inputed
+
+
+
     return(
         <div>
-            <ul >
-                <li>my home page</li>
-            </ul>
+        <form onsubmit={handleSubmit}>
+            <input type="text" placeholder="Enter new todo" onChange={handleChange} name={newTodo} value={newTodo}/>
+            <button type="submit"  onsubmit={handleSubmit} onclick="">submit</button>
+        </form>
             
-            <Todolist />
+            <Todolist todo={todo}/>
         </div>
+    )
+}
+
+
+
+function footer(){
+    return(
+<div>
+        <footer/>
+    
+    </div>
     )
 }
 export default home;
